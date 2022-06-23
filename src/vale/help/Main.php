30567ab040a2fa368063a0d1b4647d0d84de04a7
kaleb <92319390+vaqle@users.noreply.github.com>
2022-06-23 15:44:21 -0400

    Create Main.php

---
+++ src/vale/help/Main.php
@@ -0,0 +1,33 @@
+<?php
+namespace vale\help;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\Server;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\SingletonTrait;
+
+class Main extends PluginBase
+{
+	use SingletonTrait;
+
+	public function onLoad(): void
+	{
+		$this->saveResource("config.yml");
+		self::setInstance($this);
+	}
+
+	public function onEnable(): void
+	{
+		$map = Server::getInstance()->getCommandMap();
+		$map->unregister($map->getCommand("help"));
+		$command = new class("Help") extends Command{
+			public function execute(CommandSender $sender, string $commandLabel, array $args): bool{
+				$sender->sendMessage(join("\n", Main::getInstance()->getConfig()->get("help") ?? []));
+				return true;
+			}
+		};
+		$help = $command;
+		$help->setAliases(["help"]);
+		$map->register("xd", $help);
+	}
+}
